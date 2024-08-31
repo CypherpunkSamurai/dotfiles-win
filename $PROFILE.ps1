53e1aeefdb87b7570c3bfad9deac30c05a4d94c4
Cypherpunk Samurai <rakeshchow202@gmail.com>
2024-08-31 21:26:47 +0530

    Create $PROFILE.ps1

---
+++ $PROFILE.ps1
@@ -0,0 +1,54 @@
+# shell config (init)
+$THEME="onehalf.minimal"
+oh-my-posh init pwsh --config "C:\Users\$env:USERNAME\AppData\Local\Programs\oh-my-posh\themes\$THEME.omp.json" | Invoke-Expression
+
+# Type Get-PoshThemes to get themes
+# theme files are located at C:\Users\rc202\AppData\Local\Programs\oh-my-posh\themes
+# Minimal Themes
+# - ~/1_shell.omp.json
+# - amro
+# - atomic
+# - atomicBit
+# - catppuccin_frappe
+# - catppuccin_macchiato
+# - catppuccin_mocha
+# - cert
+# - cloud-context
+# - emodipt-extend
+# - emodipt
+# - gruvbox
+# - honukai
+# - hotstick.minimal
+# - hul10
+# - huvix
+# - iterm2
+# - jandedobbeleer
+# - jonnychipz
+# - json
+# - kali
+# - lambda
+# - lambdageneration
+# - marcduiker
+# - material
+# - neko
+# - nordtron
+# - nu4a
+# - onehalf.minimal
+# - paradox
+# - patriksvensson
+# - peru
+# - powerline
+# - pure
+# - spaceship
+# - star
+# - stelbent-compact.minimal
+# - tiwahu
+# - tokyo
+# - tokyonight_storm
+# - uew
+# - wopian
+# - ys
+
+# run using
+# . $PROFILE
+
